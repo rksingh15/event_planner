c8791449888d8dc00ab3290b9306a1c8a72623e7
rksingh15 <ritiksinghkushwaha06@gmail.com>
2026-02-11 10:46:10 +0530

    adding data

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+    
+     const Footer = () => {
+         return (
+             <>
+      <footer>
+      <p>&copy; Event Planner Organization. All rights reserved.</p>
+      </footer>
+             </>
+         )}
+
+     export default Footer
